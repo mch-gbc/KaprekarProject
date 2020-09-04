482affd90b6b348b2278dc15d4f0c32e45cd0cab
Malaki Holmes <mholmes15@huskers.unl.edu>
2020-09-04 14:54:58 -0500

    Fixed Java Syntax

--- src/unl/cse/kaprekar/KaprekarUtils.java
+++ src/unl/cse/kaprekar/KaprekarUtils.java
@@ -22,26 +22,29 @@ public class KaprekarUtils {
 	 * @return <code>true</code> if <code>n</code> is a Kaprekar Number, 
 	 * <code>false</code> otherwise.
 	 */
-	public static boolean isKaprekar(int n) {
+	public static boolean isKaprekar(int n)
+	{
 		
-		if(n < 1); {
-			return 0;
-		} 
-		
+		if(n < 1); 
+			{
+				return false;
+			} 
+		int i;
 		//we use a long to accommodate larger squares
-		long square = n * n;
-		int numDigits = (int) Math.log10(n) + 1;
+		long square = (n * n);
+		int numDigits = (int) Math.log10(square) + 1;
 		long modulus = 1;
 		long first, second;
-		for(int i=1; i<=numDigits; i++) {
+		for(int i=1; i<=numDigits; i++) 
+			{
 			modulus *= 10;
-			frist = square / modlus;
+			first = square / modulus;
 			second = square % modulus;
-			if(first > 0 &&
-			   first + second = n) {
-				return true;
+				if(first > 0 && first + second == n) 
+					{
+						return true;
+					}
 			}
-		}
 		return false;
 		
 	}
